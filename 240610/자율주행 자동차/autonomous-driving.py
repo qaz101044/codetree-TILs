260ec1069af12ebe61a942b9fff308810f9c57b4
Kim Hong Hee <105047982+qaz101044@users.noreply.github.com>
2024-06-10 20:28:14 +0900

    Title: 자율주행 자동차, Time: 5055ms, Memory: 25MB, Status: Time Limit Exceed - CodeTree

--- 240610/자율주행 자동차/autonomous-driving.py
+++ 240610/자율주행 자동차/autonomous-driving.py
@@ -7,7 +7,7 @@ dx = [-1,0,1,0]
 dy = [0,1,0,-1]
 
 ## 자율 주행 자동차 초기 조건
-sx, sy, d = map(int,input().split())
+x, y, d = map(int,input().split())
 
 board = []
 for i in range(N) :
@@ -16,25 +16,53 @@ for i in range(N) :
 visited = [[0 for j in range(M)] for i in range(N)]
 
 q = deque()
-q.append((sx,sy))
-visited[sx][sy] = 1
+q.append((x,y))
+visited[x][y] = 1
+
+break_point = 0
 cnt = 0
 
-while q :
-    x,y = q.popleft()
+while break_point == 0 :
 
-    for k in range(4) :
-        
-        nx = x + dx[k]
-        ny = y + dy[k]
+    d -= 1
+    if d < 0 :
+        d += 4
+    
+    nx = x + dx[d]
+    ny = y + dy[d]
 
-        if 0 <= nx < N and 0 <= ny < M and board[nx][ny] == 0 and visited[nx][ny] == 0 :
-            q.append((nx,ny))
-            visited[nx][ny] = 1
+    if board[nx][ny] == 0 and visited[nx][ny] == 0 :
+        cnt = 0
+        x = nx
+        y = ny
+        visited[nx][ny] = 1
 
+    else :
+        if cnt < 4 :
+            cnt += 1
+    
+    ## 3 단계
+    
+    d -= 2
+    if d < 0 :
+        d += 4
+    
+    bx = x + dx[d]
+    by = y + dy[d]
+
+    if board[bx][by] == 1 :
+        break_point = 1
+    
+    else :
+        cnt = 0
+        x = bx
+        y = by
+        visited[bx][by] = 1
+
+answer = 0
 for i in range(N) :
     for j in range(M) :
         if visited[i][j] == 1 :
-            cnt += 1
+            answer += 1
 
-print(cnt)
+print(answer)
