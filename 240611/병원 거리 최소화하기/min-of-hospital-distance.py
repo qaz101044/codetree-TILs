4fb93c16f257ece601acbf51bc88ec61b8e8fc57
Kim Hong Hee <105047982+qaz101044@users.noreply.github.com>
2024-06-11 00:08:11 +0900

    Title: 병원 거리 최소화하기, Time: 248ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 240611/병원 거리 최소화하기/min-of-hospital-distance.py
@@ -0,0 +1,35 @@
+from itertools import combinations
+
+# 0 = 빈칸, 1 = 사람, 2 = 병원
+N,M = map(int,input().split())
+
+board = []
+for i in range(N) :
+    board.append(list(map(int,input().split())))
+
+h = []
+p = []
+
+for i in range(N) :
+    for j in range(N) :
+
+        if board[i][j] == 2 :
+            h.append((i,j))
+
+        elif board[i][j] == 1 :
+            p.append((i,j))
+
+answer = 987654321
+for i in combinations(h,M) :
+    total = 0
+
+    for px,py in p :
+        distance_min = 987654321
+
+        for x,y in i :
+            d = abs(x-px) + abs(y-py)
+            distance_min = min(distance_min,d)
+        total += distance_min
+    
+    answer = min(total,answer)
+print(answer)
